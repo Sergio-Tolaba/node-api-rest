e0d0b94a8409aeaf6e816cdc7f98fe777fac9f61
Sergio-Tolaba <tolser2019@gmail.com>
2025-06-13 23:12:24 -0300

    C:/Program Files/Git/products=>res.send(products) o res.json(products)

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+
+const products = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hola API REST</h1>");
 });
+
+app.get("/products", (req, res) => {
+  res.json(products); // Mejor usar .json()
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
