5556911cfc0863294d0891b2d59f840e1b7bbf0e
Sergio-Tolaba <tolser2019@gmail.com>
2025-06-13 21:05:53 -0300

    npm run dev=>nodemon index.js=>[nodemon] starting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hola API !!</h1>");
+  res.send("<h1>Hola API REST</h1>");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
